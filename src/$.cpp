104350444023a1bd2ac9e2828f2aa62814139b17
r03922093 <r03922093@linux3.csie.ntu.edu.tw>
2015-06-22 14:29:47 +0800

    add color and cd function from final.cpp to $.cpp

--- src/$.cpp
+++ src/$.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <unistd.h>
 #include "HistoryWindow.h"
 
 #define N 80
@@ -26,20 +27,22 @@ char* my_generator(const char*,int);
 static int backward_kill_word_to_cmd (int, int);
 char *dupstr (char*);
 void *xmalloc (int);
-void history_init(char*);
+void home_directory(void);
+char *change_directory(char *);
 
-const char* cmd [] ={ "hello", "world", "hell" ,"word", "quit", " " };
-char* his [N];
-
-//char* buffer;
 HistoryWindow* h_win;
 vector< pair<string, float> > list;
+
+char homedir[300];
 
 int main(int argc, char* argv[])
 {
 	int ret;
 	char *buf;
-	//buffer = (char *) malloc(sizeof(char)*200);
+
+	home_directory();
+	char curdir[300];
+
 	rl_attempted_completion_function = my_completion;
 	rl_bind_key('\t', rl_complete);
 	rl_bind_keyseq("\\C-k", backward_kill_word_to_cmd);
@@ -47,12 +50,12 @@ int main(int argc, char* argv[])
 	h_win = new HistoryWindow(argv[1], N, BACKSIZE);
 	h_win->show_window();
 
+	getcwd(curdir,295);
+	fprintf(stdout,"\n\33[1;36mcurrent directory : %s\33[m",curdir);
 	while((buf = readline("\n== SmartShell ==> "))!=NULL) { //TODO working directory
 		//enable auto-complete
 		rl_bind_key('\t',rl_complete);
 		rl_bind_keyseq("\\C-k", backward_kill_word_to_cmd);
-		//rl_bind_key('\t',rl_menu_complete);
-		printf("cmd [%s]\n",buf);
 
 		if (strcmp(buf,"exit")==0)
 			break;
@@ -61,9 +64,18 @@ int main(int argc, char* argv[])
 			//add_history(buf);
 			h_win->add_entry(string(buf));
 		}
+		if (buf[0]=='c' && buf[1]=='d'){
+			if((chdir(change_directory(buf)))<0){
+				fprintf(stdout,"cd ERROR!\n");
+			}
+		}
+		else{
+			cout << buf << endl;
+			system(buf);
+		}
 
-		//execute the entered command
-		system(buf);
+		getcwd(curdir,295);
+		fprintf(stdout,"\n\33[1;36mcurrent directory : %s\33[m",curdir);
 	}
 
 	//free(buf);
@@ -72,10 +84,7 @@ int main(int argc, char* argv[])
 }
 static char** my_completion( const char * text , int start,  int end)
 {
-	char **matches;
-	int ret;
-	char str[5]="test"; 
-	matches = (char **)NULL;
+	char **matches = (char **)NULL;
 	//printf("\n[[%s]] %d,%d\n", text, start, end);  //text contains only ONE argument (seperated by blanks)
 	
 	//generate candidate list before completion
@@ -93,7 +102,7 @@ static char** my_completion( const char * text , int start,  int end)
 		cout.precision(2);
 		for(vector< pair<string, float> >::iterator it = list.begin(); it != list.end(); ++it)
 		{
-			cout << "\t" << (*it).first << "\t" << fixed << (*it).second * 100 << "%" << endl;
+			cout << "\t\33[1;33m" << (*it).first << "\33[m\t" << fixed << (*it).second * 100 << "%" << endl;
 		}
 	}
 
@@ -155,20 +164,6 @@ char* my_generator(const char* text, int state)
 	return ((char *)NULL);
 }
 
-void history_init(char* hist_file){
-	register int i;
-	register HIST_ENTRY **the_list;
-	//read_history ("/home/lilian/.bash_history");
-	//read_history_range ("/home/lilian/.bash_history", 490, 500);
-	read_history(hist_file);
-	the_list = history_list ();
-	for(i=0; i<N; i++){
-		his[i] = (char*) xmalloc(strlen(the_list[i]->line) + 1);
-		strcpy(his[i], the_list[i]->line);
-		//printf("%s\n", his[i]);
-	}
-}
-
 static int backward_kill_word_to_cmd (int count, int ignore)
 {
 	// in rl_backward_kill_word
@@ -215,4 +210,34 @@ void * xmalloc (int size)
 	}
 
 	return buf;
+}
+
+void home_directory(void){
+	FILE *fd=popen("echo $HOME","r");
+	fgets(homedir,295,fd);
+	homedir[strlen(homedir)-1]='\0';
+}
+
+char *change_directory(char * command){
+	int i;
+	int my_len;
+	char *newdir=(char*)malloc(300);
+
+	if(command[3]=='~'){
+		newdir[0]='\0';
+		strcpy(newdir,homedir);
+		my_len=strlen(homedir);
+		for(i=4;i<strlen(command);i++){
+			newdir[my_len+i-4]=command[i];
+		}
+		newdir[my_len+i-4]='\0';
+		return newdir;
+	}
+	else{
+		for(i=3;i<strlen(command);i++){
+			newdir[i-3]=command[i];
+		}
+		newdir[i-3]='\0';
+		return newdir;
+	}
 }
